75880505331f9d2a5ecb6c1b91ad319053f4b8e8
T-cordoba <tommy.cormo@gmail.com>
2024-02-14 13:45:31 -0500

    Defined classes and methods

--- clases/Figuras.py
+++ clases/Figuras.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Punto:
     def __init__(self, x: int, y: int):
         self.x: int = x
@@ -8,3 +11,13 @@ class Circulo:
     def __init__(self, radio: int, centro: Punto):
         self.radio: int = radio
         self.centro: Punto = centro
+
+    def calcular_area(self) -> float:
+        return  math.pi * self.radio ** 2
+
+    def esta_en_el_circulo(self, puntodado: Punto):
+        self.puntodado: Punto = puntodado
+        if (self.puntodado.x-self.centro.x)**2 + (self.puntodado.y-self.centro.y)**2 <= self.radio**2:
+            return True
+        else:
+            return False
